02b731cfafb3d2aefba7b9bee8ff719ccae50b0d
YUUKIToriyama <ytoriyama@torisfactory.com>
2021-06-26 01:32:44 +0900

    CLIフレームワークを使ってみる

--- cli_sample/src/main.rs
+++ cli_sample/src/main.rs
@@ -1,6 +1,31 @@
-use std::env;
+use clap::{App, Arg};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    let matches = App::new("cli sample")
+        .version("0.9.0")
+        .author("Y.Toriyama")
+        .arg(
+            Arg::new("message")
+                .about("メッセージ")
+                .short('m')
+                .long("message")
+                .value_name("MESSAGE")
+                .required(false),
+        )
+        .arg(
+            Arg::new("verbose")
+                .about("詳細を表示します")
+                .short('v')
+                .long("verbose")
+                .required(false),
+        );
+    let parsed = matches.get_matches();
+    match parsed.value_of("message") {
+        Some(message) => println!("{}", message),
+        None => println!("No message"),
+    }
+
+    if parsed.is_present("verbose") {
+        println!("Verbose mode.");
+    }
 }
