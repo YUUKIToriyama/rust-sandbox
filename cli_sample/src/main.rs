5d87a26cc07e13fd70551e77481a545978f0d8a2
YUUKIToriyama <ytoriyama@torisfactory.com>
2021-06-26 09:23:05 +0900

    update cli_sample

--- cli_sample/src/main.rs
+++ cli_sample/src/main.rs
@@ -1,15 +1,25 @@
 use clap::{App, Arg};
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 fn main() {
     let matches = App::new("cli sample")
         .version("0.9.0")
         .author("Y.Toriyama")
         .arg(
-            Arg::new("message")
-                .about("メッセージ")
-                .short('m')
-                .long("message")
-                .value_name("MESSAGE")
+            Arg::new("formula")
+                .about("数式")
+                .short('f')
+                .long("formula")
+                .value_name("FORMULA")
+                .required(false),
+        )
+        .arg(
+            Arg::new("input")
+                .about("ファイル入力")
+                .short('i')
+                .long("input-file")
+                .value_name("FILE")
                 .required(false),
         )
         .arg(
@@ -18,14 +28,25 @@ fn main() {
                 .short('v')
                 .long("verbose")
                 .required(false),
-        );
-    let parsed = matches.get_matches();
-    match parsed.value_of("message") {
-        Some(message) => println!("{}", message),
-        None => println!("No message"),
+        )
+        .get_matches();
+
+    match matches.value_of("input") {
+        Some(filename) => load_file(&filename),
+        None => println!("ファイルが指定されていません"),
     }
 
-    if parsed.is_present("verbose") {
+    if matches.is_present("verbose") {
         println!("Verbose mode.");
+    }
+}
+
+fn load_file(path: &str) {
+    let f = File::open(path).unwrap();
+    let reader = BufReader::new(f);
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        println!("{}", line);
     }
 }
