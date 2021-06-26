7003bb855b72cef85dca5ee5a1ba902d31214bed
YUUKIToriyama <github@toriyama.dev>
2021-06-26 20:20:01 +0900

    標準入力からのデータ入力に対応

--- cli_sample/src/main.rs
+++ cli_sample/src/main.rs
@@ -1,6 +1,6 @@
 use clap::{App, Arg};
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{stdin, BufRead, BufReader};
 
 fn main() {
     let matches = App::new("cli sample")
@@ -32,21 +32,28 @@ fn main() {
         .get_matches();
 
     match matches.value_of("input") {
-        Some(filename) => load_file(&filename),
-        None => println!("ファイルが指定されていません"),
-    }
-
-    if matches.is_present("verbose") {
-        println!("Verbose mode.");
+        // ファイル名が指定されている場合
+        Some(filename) => {
+            let f = File::open(filename).unwrap();
+            let reader = BufReader::new(f);
+            run(reader, matches.is_present("verbose"));
+        }
+        // 指定されていない場合
+        None => {
+            let stdin = stdin();
+            let reader = stdin.lock();
+            run(reader, matches.is_present("verbose"));
+        }
     }
 }
 
-fn load_file(path: &str) {
-    let f = File::open(path).unwrap();
-    let reader = BufReader::new(f);
-
+// R: BufReadを実装している任意の型。BufReader<File>型、StdinLock型を想定
+fn run<R: BufRead>(reader: R, verbose: bool) {
     for line in reader.lines() {
         let line = line.unwrap();
         println!("{}", line);
+    }
+    if verbose {
+        println!("Verbose mode.");
     }
 }
